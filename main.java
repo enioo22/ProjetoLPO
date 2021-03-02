737533cbbcd402ed2e7e562ec100f3b17d9a1197
assistente22 <79934130+assistente22@users.noreply.github.com>
2021-03-02 16:12:26 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,16 @@
 package projetoLPO;
 
-public class main {
 
+public class main {
+	public static void main(String[] args){
+		
+		Partida partida = new Partida();
+		partida.inicializarJogadores();
+		for(int i = 0; i<20; i++) {
+			partida.fazerJogada();
+		}
+	}
+	
+		
+	
 }
