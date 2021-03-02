5eed5b2c19eb582bba121a679e4217af6c88fd5a
assistente22 <79934130+assistente22@users.noreply.github.com>
2021-03-02 16:12:01 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -2,12 +2,17 @@ package projetoLPO;
 import java.util.Scanner;
 
 public class Jogador {
-    private String nome;
+    private String nome = "0";
+    private char pontuacao = 0;
 
     public String chamarNome() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Infrome o nome do Jogador");
-        nome = entrada.next();
+        System.out.println("Informe o nome do Jogador");
+        this.nome = entrada.next();
         return nome;
+    }
+    
+    public void fezPonto() {
+    	this.pontuacao++;
     }
 }
