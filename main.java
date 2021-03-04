9fae3908f8adc15a77c34a718bc8f4f245b3c3f7
assistente22 <79934130+assistente22@users.noreply.github.com>
2021-03-04 17:16:36 -0300

    Update main.java

--- main.java
+++ main.java
@@ -5,12 +5,13 @@ public class main {
 	public static void main(String[] args){
 		
 		Partida partida = new Partida();
-		Partida.inicializarJogadores();
+		partida.inicializarJogadores();
 		for(int i = 0; i<20; i++) {
 			System.out.println("turno " + (i+1));
 			partida.fazerJogada();
 		}
-		System.out.println("gg");
+		
+		//System.out.println("gg");
 	}
 	
 		
