11522122348600a44d2a515690a546771faf6b28
assistente22 <79934130+assistente22@users.noreply.github.com>
2021-03-04 17:17:09 -0300

    Update Tabuleiro.java

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -2,7 +2,9 @@ package projetoLPO;
 
 public class Tabuleiro {
 	 private static int[][] tabuleiro = {{1, 2, 3, 4}, {1, 2, 3, 4}, {4, 3, 2, 1}, {4, 3, 2, 1} };
-	    public static boolean fazerJogada(int x1, int y1, int x2, int y2) {
+	 
+	 
+	    public boolean fazerJogada(int x1, int y1, int x2, int y2) {
 	    	return tabuleiro[x1][y1] == tabuleiro[x2][y2];
 	    }
 	    
