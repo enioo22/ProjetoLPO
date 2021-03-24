cdb2f471a1aaeede4bd71e436c0fca9d0581564c
assistente22 <79934130+assistente22@users.noreply.github.com>
2021-03-23 23:57:53 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,18 @@
+package projetoLPO;
+
+public class Pessoa {
+	private String nome;
+	public Pessoa(String nome) {
+		this.nome = nome;
+	}
+	public String toString() {
+		return (" - O nome do jogador é " +nome);
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
