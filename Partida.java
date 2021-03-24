61c83952e0921ed196ad34dcd852a7f9ee38f436
assistente22 <79934130+assistente22@users.noreply.github.com>
2021-03-23 23:57:21 -0300

    Update Partida.java

--- Partida.java
+++ Partida.java
@@ -2,22 +2,24 @@ package projetoLPO;
 import java.util.Scanner;
 
 public class Partida {
-	private static Jogador[] jogador = new Jogador[2];{
-	jogador[0] = new Jogador();
-	jogador[1] = new Jogador();
-	}
+	private static Jogador[] jogador = new Jogador[2];
 	private static Tabuleiro tabuleiro = new Tabuleiro();
 	private static char jogadorAtual = 0;
+	Scanner scan = new Scanner(System.in);
+	
 	
 	public void inicializarJogadores() {
-		jogador[0].chamarNome();
-		jogador[1].chamarNome();
+		String nome1 = scan.nextLine();
+		String nome2 = scan.nextLine();
+		jogador[0] = new Jogador(nome1, 1);
+		jogador[1] = new Jogador(nome2, 2);
+		System.out.println(jogador[0].toString());
 	}
 	
 	public void fazerJogada() {
 		boolean acerto;
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Jogador da Vez: " + jogador[jogadorAtual].nome);
+
+		System.out.println("Jogador da Vez: " + jogador[jogadorAtual].getNome());
 		
 		System.out.println("X da primeira peça: ");
 		int x1 = scan.nextInt();
@@ -29,7 +31,7 @@ public class Partida {
 		int y2 = scan.nextInt();
 		acerto = tabuleiro.fazerJogada(x1, y1, x2, y2); //retorna o resultado da jogada,  true == acerto
 		
-		System.out.println("jogador " + jogador[jogadorAtual].nome + " fez a jogada " + x1 + " e " + y1 + " e depois a proxima jogada " + x2 + " e " + y2);
+		System.out.println("jogador " + jogador[jogadorAtual].getNome() + " fez a jogada " + x1 + " e " + y1 + " e depois a proxima jogada " + x2 + " e " + y2);
 		
 		if(acerto == false && jogadorAtual == 0) jogadorAtual = 1; //mudança de player se errou na jogada
 		else if(acerto == false && jogadorAtual == 1) jogadorAtual = 0;
@@ -37,5 +39,3 @@ public class Partida {
 		
 	}
 }
-
-
