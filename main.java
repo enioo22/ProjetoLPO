68bd57becc397add023fa975de76425860852e65
assistente22 <79934130+assistente22@users.noreply.github.com>
2021-03-23 23:56:13 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,13 @@
 package projetoLPO;
 
+// GRUPO: Ênio Ferreira && Bernardo Cabral
 
 public class main {
 	public static void main(String[] args){
 		
 		Partida partida = new Partida();
 		partida.inicializarJogadores();
+		
 		for(int i = 0; i<20; i++) {
 			System.out.println("turno " + (i+1));
 			partida.fazerJogada();
