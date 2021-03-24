763123cd5b094fe3590c78799e1ef4f9cccecb1c
assistente22 <79934130+assistente22@users.noreply.github.com>
2021-03-23 23:56:46 -0300

    Update Tabuleiro.java

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,11 +1,16 @@
 package projetoLPO;
 
 public class Tabuleiro {
-	 private static int[][] tabuleiro = {{1, 2, 3, 4}, {1, 2, 3, 4}, {4, 3, 2, 1}, {4, 3, 2, 1} };
+	 private String[][] tabuleiro;
+	 
+	 public Tabuleiro(){
+		 this.tabuleiro = new String[][] {{"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"4", "3", "2", "1"}, {"4", "3", "2", "1"}};
+		 
+	 }
 	 
 	 
 	    public boolean fazerJogada(int x1, int y1, int x2, int y2) {
-	    	return tabuleiro[x1][y1] == tabuleiro[x2][y2];
+	    	return tabuleiro[x1][y1].equals(tabuleiro[x2][y2]);
 	    }
 	    
 
