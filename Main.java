96694722acf8188f8d2598f34350c780a6bb3595
assistente22 <79934130+assistente22@users.noreply.github.com>
2021-04-22 15:43:22 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,13 +5,16 @@ import br.com.poli.memoriapog.partida.Partida;
 import br.com.poli.memoriapog.player.Jogador;
 import java.util.Scanner;
 
-// GRUPO: Ênio Ferreira && Bernardo Cabral
+// GRUPO: ÃŠnio Ferreira && Bernardo Cabral
 
 public class Main {
 	
 	public static void main(final String[] args){
 		
-
+		boolean continuarPartida = true, erroDificuldade = true;
+		int dificuldade;
+		
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		System.out.println("digite o nome do primeiro Jogador");
 		String nome1 = scan.nextLine();
@@ -21,13 +24,27 @@ public class Main {
 		Jogador jogador1 = new Jogador(nome1, 1);
 		Jogador jogador2 = new Jogador(nome2, 2);
 		
-		System.out.println("            Dificuldade \n1 - Facil || 2 - Medio || 3 - Dificil ");
-		int dificuldade = scan.nextInt();
+		do { //garantir que o input seja valido
+			System.out.println("            Dificuldade \n1 - Facil || 2 - Medio || 3 - Dificil ");
+			System.out.println("    (4x4) ||    (6x6)  ||     (8x8)   ");
+			dificuldade = scan.nextInt();
+			if(dificuldade<1 || dificuldade>3) {
+				erroDificuldade = true;
+				System.out.println("DIFICULDADE INVALIDA");
+			}
+			else erroDificuldade = false;
+			
+		}while(erroDificuldade);
+		
+		
 		Partida partida = new Partida(dificuldade, jogador1, jogador2);
 		
-		for(int i = 0; i<20; i++) {
-			System.out.println("turno " + (i+1));
+		while(continuarPartida) {
+			int i = 1;
+			System.out.println("turno " + (i));
+			i++;
 			partida.fazerJogada();
+			continuarPartida = partida.getContinuarPartida();
 		}
 		
 		//System.out.println("gg");
